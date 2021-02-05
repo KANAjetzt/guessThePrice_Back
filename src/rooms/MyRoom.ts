77a5ada2f9da8caa769e96fa533997e2d3ee34d7
KANAjetzt <kai@kana.jetzt>
2021-02-05 13:33:13 +0100

    👀 There is something called methods 👀

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -4,13 +4,12 @@ import { GameState, PlayerState } from './schema/GameState'
 import { getOne } from '../DB/controllers/factory'
 import ProductModel from '../DB/models/product'
 
-// 🛠 WIP 🛠: Get 10 random DB entries
-// This 10 entries are in row - so there will be the same products in a row
-
 const getRandomArbitrary = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
+// 🛠 WIP 🛠: Get 10 random DB entries
+// This 10 entries are in row - so there will be the same products in a row
 const getProducts = async () => {
   const productCount = await ProductModel.estimatedDocumentCount()
 
@@ -21,22 +20,78 @@ const getProducts = async () => {
   return products
 }
 
-// Calculate player score
-const getScore = (productPrice: number, playerGuessedPrice: number) => {
-  let difference = productPrice - playerGuessedPrice
-  if (difference < 0) difference = difference * -1
-
-  // percentage to the actual price
-  const percentage = (productPrice / 100 / difference).toFixed(2)
-
-  // create score
-  const score = (100 - parseInt(percentage.split('.')[1], 10)) * 10
-  console.log(score)
-
-  return score
-}
-
 export class MyRoom extends Room {
+  // Calculate player score
+  getScore(productPrice: number, playerGuessedPrice: number) {
+    console.log(productPrice, playerGuessedPrice)
+    let difference = productPrice - playerGuessedPrice
+    if (difference < 0) difference = difference * -1
+    // percentage to the actual price
+    const percentage = (productPrice / 100 / difference).toFixed(2)
+    // create score
+    return (100 - parseInt(percentage.split('.')[1], 10)) * 10
+  }
+
+  // Get product from products state
+  getProduct(products: Array<Product>, currentRound: number) {
+    return products[currentRound]
+  }
+
+  handlePlayerGuess(client: any, message: any, players: Array<PlayerState>) {
+    console.log(client.sessionId, "sent 'action' message: ", message)
+
+    // Get the index of the player that guessed the price
+    const index = players.findIndex((e: any) => e.id === client.sessionId)
+
+    // Update guessedPrice in player state
+    players[index].guessedPrice = message.guessedPrice
+
+    // Get players that didn't guessed yet
+    const playersNotGuessed = players.filter((e: any) => {
+      return e.guessedPrice === 0
+    })
+
+    // Check if all players guessed a price
+    if (playersNotGuessed.length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  // Start the round
+  startRound() {
+    // Reset round state
+    this.state.roundEnded = false
+    this.state.isRoundScoreCalculated = false
+
+    // Update currentRound state
+    this.state.currentRound++
+
+    // Reset player guessedPrice
+
+    // Set new currentProduct
+    this.state.currentProduct = this.getProduct(
+      this.state.products,
+      this.state.currentRound
+    )
+  }
+
+  endRound(players: Array<PlayerState>) {
+    // Calculate player scores
+    if (!this.state.isRoundScoreCalculated) {
+      players.forEach((player: any) => {
+        const score = this.getScore(
+          this.state.currentProduct.price,
+          player.guessedPrice
+        )
+        console.log(score)
+        player.score = score
+      })
+      this.state.isRoundScoreCalculated = true
+    }
+  }
+
   onCreate(options: any) {
     // Initialize GameState
     this.setState(new GameState())
@@ -70,45 +125,25 @@ export class MyRoom extends Room {
     })()
 
     this.setSimulationInterval(() => {
-      // Check for product update
-
       // Check if round has ended
       if (this.state.roundEnded) {
-        // check if scores where calculated before for this round
-        if (this.state.isRoundScoreCalculated) return
-        // calculate score for players
-        console.log('--- calculate player score ---')
-        this.state.playerStates.forEach((player: any) => {
-          player.score = getScore(
-            this.state.currentProduct.productPrice,
-            player.guessedPrice
-          )
-        })
-
-        this.state.isRoundScoreCalculated = true
+        this.endRound(this.state.playerStates)
+      }
+      if (this.state.isRoundScoreCalculated) {
+        this.startRound()
       }
     })
 
     this.onMessage('guessedPrice', (client, message) => {
-      console.log(client.sessionId, "sent 'action' message: ", message)
-
-      // Get the index of the player that guessed the price
-      const index = this.state.playerStates.findIndex(
-        (e: any) => e.id === client.sessionId
+      // Updates guessedPrice in player state
+      // Returns true if all Players guessed a price
+      const allPlayersGuessed = this.handlePlayerGuess(
+        client,
+        message,
+        this.state.playerStates
       )
-
-      // Update guessedPrice in player state
-      this.state.playerStates[index].guessedPrice = message.guessedPrice
-
-      // Get players that didn't guessed yet
-      const playersNotGuessed = this.state.playerStates.filter((e: any) => {
-        return e.guessedPrice === 0
-      })
-
-      console.log(playersNotGuessed)
-
-      // Check if all players guessed a price
-      if (playersNotGuessed.length === 0) {
+      // If all players guessed a price end the round
+      if (allPlayersGuessed) {
         this.state.roundEnded = true
       }
     })
